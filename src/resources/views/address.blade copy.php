a6e3bd10e94ac73789284c0dfd15739b0b326a80
genshi <genshi@genshinoMacBook-Pro.local>
2022-11-02 23:21:20 +0900

    bladeにsection適用

--- src/resources/views/address.blade copy.php
+++ src/resources/views/address.blade copy.php
@@ -1,122 +1,63 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('/css/detail.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownBland" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            BLAND
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownBland">
-                            <li><a class="dropdown-item" href="#">Bland A</a></li>
-                            <li><a class="dropdown-item" href="#">Bland B</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownCategory" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            CATEGORY
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownCategory">
-                            <li><a class="dropdown-item" href="#">Category A</a></li>
-                            <li><a class="dropdown-item" href="#">Category B</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                </div>
-            </div>
-        </nav>
-    </header>
+@extends('layouts.common')
 
-    <main>
-        <div class="container">
-            <div class="mb-3 row">
-                <label for="name" class="col-sm-2 col-form-label">お名前</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="name" name="name" placeholder="{{ $user->name }}">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="email" class="col-sm-2 col-form-label">Eメールアドレス</label>
-                <div class="col-sm-7">
-                    <input type="email" class="form-control" id="email" name="email">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="post_code" class="col-sm-2 col-form-label">郵便番号</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="post_code" name="post_code">
-                </div>
-                <div class="col-sm-1" ></div>
-                <div class="col-sm-4">
-                    <a class="btn btn-primary" href="">住所検索</a>
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="prefecture" class="col-sm-2 col-form-label">都道府県</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="prefecture" name="prefecture">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="city" class="col-sm-2 col-form-label">市区町村</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="city" name="city">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="address" class="col-sm-2 col-form-label">それ以降の住所</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="address" name="address">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="tel" class="col-sm-2 col-form-label">電話番号</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="tel" name="tel">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <div class="col-sm-3">
-                    <a class="btn btn-primary" href="">ショッピングに戻る</a>
-                </div>
-                <div class="col-sm-3">
-                    <a class="btn btn-primary" href="">カートに戻る</a>
-                </div>
-                <div class="col-sm-3">
-                    <a class="btn btn-primary" href="">購入する</a>
-                </div>
-            </div>
+@section('main')
+<div class="container">
+    <div class="mb-3 row">
+        <label for="name" class="col-sm-2 col-form-label">お名前</label>
+        <div class="col-sm-7">
+            <input type="text" class="form-control" id="name" name="name" placeholder="{{ $user->name }}">
         </div>
-
-    </main>
-
-    <footer id="footer" class="wrapper">
-      <p class="copyright">© TOTALLY</p>
-    </footer>
-
-</body>
-</html>
+    </div>
+    <div class="mb-3 row">
+        <label for="email" class="col-sm-2 col-form-label">Eメールアドレス</label>
+        <div class="col-sm-7">
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="post_code" class="col-sm-2 col-form-label">郵便番号</label>
+        <div class="col-sm-3">
+            <input type="text" class="form-control" id="post_code" name="post_code">
+        </div>
+        <div class="col-sm-1" ></div>
+        <div class="col-sm-4">
+            <a class="btn btn-primary" href="">住所検索</a>
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="prefecture" class="col-sm-2 col-form-label">都道府県</label>
+        <div class="col-sm-7">
+            <input type="text" class="form-control" id="prefecture" name="prefecture">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="city" class="col-sm-2 col-form-label">市区町村</label>
+        <div class="col-sm-7">
+            <input type="text" class="form-control" id="city" name="city">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="address" class="col-sm-2 col-form-label">それ以降の住所</label>
+        <div class="col-sm-7">
+            <input type="text" class="form-control" id="address" name="address">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="tel" class="col-sm-2 col-form-label">電話番号</label>
+        <div class="col-sm-7">
+            <input type="text" class="form-control" id="tel" name="tel">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <div class="col-sm-3">
+            <a class="btn btn-primary" href="">ショッピングに戻る</a>
+        </div>
+        <div class="col-sm-3">
+            <a class="btn btn-primary" href="">カートに戻る</a>
+        </div>
+        <div class="col-sm-3">
+            <a class="btn btn-primary" href="">購入する</a>
+        </div>
+    </div>
+</div>
+@endsection
--- src/resources/views/list.blade.php
+++ src/resources/views/list.blade.php
@@ -1,71 +1,43 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownBland" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        BLAND
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownBland">
-                        @foreach ($brands as $brand)
-                        <li><a class="dropdown-item" href="#">{{ $brand->name }}</a></li>
-                        @endforeach
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownCategory" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        CATEGORY
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownCategory">
-                        @foreach ($categories as $category)
-                        <li><a class="dropdown-item" href="#">{{ $category->name }}</a></li>
-                        @endforeach
-                    </ul>
-                </li>
-            </ul>
-            <div>
-                <a class="btn btn-sm btn-primary" href="{{ route('account.edit') }}">購入者情報</a>
-                <a class="btn btn-sm btn-primary" href="{{ route('cart.list') }}">カート</a>
-            </div>
-            </div>
-        </div>
-    </nav>
+@extends('layouts.common')
 
-    <div class="container">
-        <h3 class="mt-5">Bland A</h3>
+@section('main')
+<div id="item" class="wrapper">
+    <div class="item-image">
+        <img src="{{ asset('/images/fashion_onepiece.png') }}" alt="">
     </div>
-    </div>
-    <div class="container">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            @foreach ($allItems as $item)
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/assets/images/fashion_onepiece.png') }}" alt="#">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $item->name }}</h5>
-                        <p class="card-text">¥{{ number_format($item->price) }}</p>
-                        <!-- <input type="hidden" name="item_id" value="{{ $item->id }}"> -->
-                        <a class="btn btn-primary" href="{{ route('product.detail', $item->id) }}">カートに入れる</a>
-                    </div>
-                </div>
-            </div>
+    <form method="POST" action="{{ route('cart.cartIn') }}">
+        @csrf
+        <div class="item-info">
+        <h1 class="item-title">{{ $item->name }}</h1>
+        <p>¥{{ number_format($item->price) }}</p>
+
+        <table class="order-table">
+            <thead>
+            <tr>
+                <th class="color">Color</th>
+                <th class="size">Size</th>
+                <th class="quantity"></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($item->itemsDetail as $itemDetail)
+            <tr>
+                <td>{{ $itemDetail->color }}</td>
+                <td>{{ $itemDetail->size }}</td>
+                <td>
+                <select name="cartInfo[]">
+                    <option value=""></option>
+                    <option value="{{ $itemDetail->id }}&1">1</option>
+                    <option value="{{ $itemDetail->id }}&2">2</option>
+                    <option value="{{ $itemDetail->id }}&3">3</option>
+                </select>
+                </td>
+            </tr>
             @endforeach
+            </tbody>
+        </table>
+        <button class="btn btn-primary submit">ADD TO CART</button>
         </div>
-    </div>
-</body>
-</html>
+    </form>
+</div>
+@endsection
