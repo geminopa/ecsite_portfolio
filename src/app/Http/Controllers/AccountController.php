28e0e036bd7baad0087c015d21f17dba385b96e6
genshi <genshi@genshinoMacBook-Pro.local>
2022-12-31 21:58:48 +0900

    try catch及びトランザクション追加”

--- src/app/Http/Controllers/AccountController.php
+++ src/app/Http/Controllers/AccountController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Services\AccountService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class AccountController extends Controller
 {
@@ -19,20 +21,27 @@ class AccountController extends Controller
 
     public function editComplete(Request $request)
     {
-        $validator = AccountService::validate($request);
-        if ($validator->fails()) {
-            return redirect()->back()->withInput()->withErrors($validator);
+        try {
+            $validator = AccountService::validate($request);
+            if ($validator->fails()) {
+                return redirect()->back()->withInput()->withErrors($validator);
+            }
+            DB::beginTransaction();
+            $user = User::find(Auth::user()->id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->post_code = $request->post_code;
+            $user->prefecture = $request->prefecture;
+            $user->city = $request->city;
+            $user->address = $request->address;
+            $user->tel = $request->tel;
+            $user->save();
+            DB::commit();
+            return redirect()->back()->with('message', 'お客様情報を更新しました');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
         }
-        $user = User::find(Auth::user()->id);
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->post_code = $request->post_code;
-        $user->prefecture = $request->prefecture;
-        $user->city = $request->city;
-        $user->address = $request->address;
-        $user->tel = $request->tel;
-
-        $user->save();
-        return redirect()->back()->with('message', 'お客様情報を更新しました');
     }
 }
--- src/app/Http/Controllers/CartController.php
+++ src/app/Http/Controllers/CartController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Item;
 use App\Models\ItemDetail;
-use App\Models\User;
 use App\Models\Cart;
 use App\Models\OrderHistory;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
@@ -24,63 +24,93 @@ class CartController extends Controller
 
     public function cartIn(Request $request)
     {
-        $itemDetailId = $request->itemDetailId;
-        $cart = Cart::where('user_id', Auth::user()->id)->where('items_detail_id', $itemDetailId)->firstOrNew();
-        if ($cart->exists) {
-            $cart->quantity += 1;
-            $cart->update();
-        } else {
-            $cart->user_id = Auth::user()->id;
-            $cart->items_detail_id = $itemDetailId;
-            $cart->quantity = 1;    //TODO:マジックナンバー控える
-            $cart->save();
+        try {
+            $itemDetailId = $request->itemDetailId;
+            $cart = Cart::where('user_id', Auth::user()->id)->where('items_detail_id', $itemDetailId)->firstOrNew();
+            DB::beginTransaction();
+            if ($cart->exists) {
+                $cart->quantity += 1;
+                $cart->update();
+            } else {
+                $cart->user_id = Auth::user()->id;
+                $cart->items_detail_id = $itemDetailId;
+                $cart->quantity = 1;    //TODO:マジックナンバー控える
+                $cart->save();
+            }
+            DB::commit();
+            return redirect()->route('cart.list');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
         }
-        return redirect()->route('cart.list');
     }
 
     public function recalculation(Request $request)
     {
-        $itemDetail = ItemDetail::where('id', $request->itemDetailId)->first();
-        if ($request->quantity > $itemDetail->stock) {
-            return redirect()->route('cart.list')->with('error', '在庫が不足しています。');
+        try {
+            $itemDetail = ItemDetail::where('id', $request->itemDetailId)->first();
+            if ($request->quantity > $itemDetail->stock) {
+                return redirect()->route('cart.list')->with('error', '在庫が不足しています。');
+            }
+            DB::beginTransaction();
+            $recalculationCart = Cart::where('id', $request->cartId)->first();
+            $recalculationCart->quantity = $request->quantity;
+            $recalculationCart->save();
+            DB::commit();
+            return redirect()->route('cart.list');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
         }
-        $recalculationCart = Cart::where('id', $request->cartId)->first();
-        $recalculationCart->quantity = $request->quantity;
-        $recalculationCart->save();
-        return redirect()->route('cart.list');
     }
 
     public function deleteCart(Request $request)
     {
-        Cart::where('id', $request->cart_id)->delete();
-        return redirect()->route('cart.list');
-        // TODO:削除後、カート一覧画面に「（商品名）を削除しました。」と表示する
+        try {
+            DB::beginTransaction();
+            Cart::where('id', $request->cart_id)->delete();
+            DB::commit();
+            return redirect()->route('cart.list');
+            // TODO:削除後、カート一覧画面に「（商品名）を削除しました。」と表示する
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
+        }
     }
 
     public function execPurchase(Request $request)
     {
-        $carts = cart::where('user_id', Auth::user()->id)->get();
-        // order_historyテーブル登録
-        foreach ($carts as $cart) {
-            if ($cart->quantity > $cart->itemDetail->stock) {
-                return redirect()->route('cart.list')->with('error', '在庫が不足しています。');
+        try {
+            $carts = cart::where('user_id', Auth::user()->id)->get();
+            // order_historyテーブル登録
+            foreach ($carts as $cart) {
+                if ($cart->quantity > $cart->itemDetail->stock) {
+                    return redirect()->route('cart.list')->with('error', '在庫が不足しています。');
+                }
+                DB::beginTransaction();
+                $orderHistory = new OrderHistory;
+                $orderHistory->user_id = $cart->user_id;
+                $orderHistory->item_id = $cart->itemDetail->item_id;
+                $orderHistory->items_detail_id = $cart->itemDetail->id;
+                $orderHistory->quantity = $cart->quantity;
+                $orderHistory->save();
+                // items_detailテーブルの在庫数を変更
+                $itemDetail = ItemDetail::where('id', $cart->items_detail_id)->first();
+                $itemDetail->sales_quantity += $cart->quantity;
+                $itemDetail->stock -= $cart->quantity;
+                $itemDetail->update();
+                $cart->delete();
+                DB::commit();
             }
-            $orderHistory = new OrderHistory;
-            $orderHistory->user_id = $cart->user_id;
-            $orderHistory->item_id = $cart->itemDetail->item_id;
-            $orderHistory->items_detail_id = $cart->itemDetail->id;
-            $orderHistory->quantity = $cart->quantity;
-            $orderHistory->save();
-            // items_detailテーブルの在庫数を変更
-            $itemDetail = ItemDetail::where('id', $cart->items_detail_id)->first();
-            $itemDetail->sales_quantity += $cart->quantity;
-            $itemDetail->stock -= $cart->quantity;
-            $itemDetail->update();
-
-            $cart->delete();
+            return redirect()->route('cart.showComplete');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e);
+            throw $e;
         }
-
-        return redirect()->route('cart.showComplete');
     }
 
     public function showComplete()
