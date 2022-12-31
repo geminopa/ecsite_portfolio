8077bf93a5cce6041ca7dddd4644a0009ae684a8
genshi <genshi@genshinoMacBook-Pro.local>
2022-12-31 21:57:56 +0900

    ビューコンポーザー使ってみた

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Category;
+use App\Models\Brand;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //compose all the views....
+        View::composer('*', function ($view) {
+            //...with this variable
+            $view->with([
+                'allCategories' => Category::all(),
+                'allBrands' => Brand::all(),
+            ]);
+        });
     }
 }
